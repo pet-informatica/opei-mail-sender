13deab8acf1964ef28ed4559b805f97b8b7b9f48
hcaula <lhtc@cin.ufpe.br>
2018-08-26 14:32:51 -0300

    Cuts PDF's

--- cut.js
+++ cut.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const scissors = require('scissors');
+const async = require('async');
+const pdf = scissors('./files/CCI.pdf');
+const allEmails = [];
 
 const search = (arr, el) => {
     let index = -1;
@@ -8,55 +12,61 @@ const search = (arr, el) => {
 
 const add = (arr, el, i, tipo) => {
     const index = search(arr, el);
-    if (index >= 0) arr[index].docs.push(i);
+    if (index >= 0) arr[index].pages.push(i + 1);
     else {
         arr.push({
             from: 'pet-informatica',
             to: el,
             tipo: tipo,
-            docs: [i]
+            pages: [i + 1]
         });
     }
 }
 
+const cut = (email, cb) => {
+    const cuted = pdf.pages(...email.pages);
+    const output = `./outputs/cut-${email.tipo}-${email.to}.pdf`;
+    const stream = fs.createWriteStream(output, { autoClose: false });
+
+    cuted.pdfStream()
+        .pipe(stream)
+        .on('finish', () => {
+            email.doc = output;
+            allEmails.push(email);
+            stream.close();
+            cb(null, output);
+        }).on('error', e => cb(e));
+}
+
 fs.readFile('./files/emails.csv', 'utf8', (err, contents) => {
     if (err) throw err;
+    console.log("Organizando objeto de emails de acordo com a entrada CSV.");
+
     const lines = contents.split('\n');
+    const alunos = [], delegados = [], instituicoes = [];
 
-    const emails = {
-        alunos: [],
-        delegados: [],
-        instituicoes: []
-    }
-
-    const docs = lines.forEach((line, i) => {
-        let email = {
-            from: 'pet-informatica'
-        }
-
+    lines.forEach((line, i) => {
         const arr = line.split(',');
         const aluno = arr[0];
         const delegado = arr[1];
         const instituicao = arr[2];
 
-        add(emails.alunos, aluno, i, 'aluno');
-        add(emails.delegados, delegado, i, 'delegado');
-        add(emails.instituicoes, instituicao, i, 'instituicao');
+        add(alunos, aluno, i, 'aluno');
+        add(delegados, delegado, i, 'delegado');
+        add(instituicoes, instituicao, i, 'instituicao');
     });
 
-    console.log(JSON.stringify(emails));
+    console.log("Cortando PDF's...");
+    let count = 1;
+    const all = alunos.concat(delegados).concat(instituicoes);
+    async.eachSeries(all, (email, cb) => {
+        cut(email, (e, output) => {
+            console.log(`Cortado: ${output} (${count++} de ${all.length})`);
+            cb(e)
+        });
+    }, e => {
+        if (e) console.log(e);
+        console.log(allEmails);
+    });
 });
 
-// /* Cortando o PDF */
-// const fs = require('fs');
-// const scissors = require('scissors');
-
-// const pdf = scissors('CCI.pdf').range(1, 2);
-
-// pdf.pdfStream()
-// .pipe(fs.createWriteStream('./cut1.pdf'))
-// .on('finish', function () {
-//     console.log("PDF cortado com sucesso.");
-// }).on('error', function (err) {
-//     throw err;
-// });
